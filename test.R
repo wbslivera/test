25c3800c5de3b99f5f29df6a4e41a1fcb98d84ab
Buddhika Livera <wbslivera@gmail.com>
2020-05-14 07:28:05 +0530

    NN iris

--- test.R
+++ test.R
@@ -1,2 +1,23 @@
-print("Local")
-print("x")
+library(neuralnet)
+iris.uci <- read.csv(url("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"), header=FALSE, col.names=c("sepal.length","sepal.width","petal.length","petal.width","species"))
+
+set.seed(101)
+size.sample <- 50
+iristrain <- iris[sample(1:nrow(iris), size.sample),] # get a training sample from iris
+nnet_iristrain <- iristrain
+
+# Binarize the categorical output
+nnet_iristrain <- cbind(nnet_iristrain, iristrain$Species == 'setosa')
+nnet_iristrain <- cbind(nnet_iristrain, iristrain$Species == 'versicolor')
+nnet_iristrain <- cbind(nnet_iristrain, iristrain$Species == 'virginica')
+
+names(nnet_iristrain)[6] <- 'setosa'
+names(nnet_iristrain)[7] <- 'versicolor'
+names(nnet_iristrain)[8] <- 'virginica'
+
+head(nnet_iristrain) 
+
+nn <- neuralnet(setosa+versicolor+virginica ~ Sepal.Length+Sepal.Width+Petal.Length+Petal.Width, data=nnet_iristrain, hidden=c(3))
+
+plot(nn)
+
